877831735f9936ea3838eb0e77496b83d7d99630
Richard Warburton <richard.warburton@gmail.com>
2012-06-10 15:42:46 +0100

    removes .patch from the filename

--- bin/check_serialuids.sh
+++ bin/check_serialuids.sh
@@ -5,6 +5,7 @@ generated_uid_file=$2
 
 grep -oR "serialVersionUID.*" $patch_dir | while read line
 do
+  line=$(echo $line | sed 's/.patch//')
   count=$(grep "$line" $generated_uid_file | wc -l)
 
   if [ $count -ne 1 ]
