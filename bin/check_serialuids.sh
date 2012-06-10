b63175d69351c9a0dda633f8b33fbb1c231e2608
Richard Warburton <richard.warburton@gmail.com>
2012-06-10 16:05:52 +0100

    fix up a bug in the script

--- bin/check_serialuids.sh
+++ bin/check_serialuids.sh
@@ -5,11 +5,14 @@ generated_uid_file=$2
 
 grep -oR "serialVersionUID.*" $patch_dir | while read line
 do
+  file=$(echo $line | cut -d':' -f 1)
   line=$(echo $line | sed 's/.patch//')
   count=$(grep "$line" $generated_uid_file | wc -l)
 
-  if [ $count -ne 1 ]
+  if [ $count -lt 1 ]
   then
     echo $line $count
+#  else
+#    echo $file
   fi
 done
